f3bbab9c1492fee4b84ea872e50c94897c9bc641
Adhi-AWS <90025213+Adhi-AWS@users.noreply.github.com>
2022-07-06 16:35:28 +0530

    Update sonar-project.js

--- sonarconfig/angular/sonar-project.js
+++ sonarconfig/angular/sonar-project.js
@@ -7,12 +7,12 @@ sonarqubeScanner({
 	'sonar.projectName': 'Jenkins-Angular',
 	'sonar.projectKey':'Jenkins-Angular',
 	'sonar.login': 'sqp_3a6655c68f498bbe02d15865b2546b2465090be9',
-      	'sonar.projectVersion':'1.0',
-	'sonar.language':'js',
-      	'sonar.sourceEncoding':'UTF-8',
-      	'sonar.sources': '.',
-	'sonar.dependencyCheck.jsonReportPath': './dependency-check-report.json',
-	'sonar.dependencyCheck.xmlReportPath': './dependency-check-report.xml',
-	'sonar.dependencyCheck.htmlReportPath': './dependency-check-report.html'
+ 	'sonar.projectVersion':'1.0',
+ 	'sonar.sourceEncoding':'UTF-8',
+ 	'sonar.sources': 'src',
+  'sonar.exclusions'='**/node_modules/**'
+  'sonar.tests'='src'
+  'sonar.test.inclusions'='**/*.spec.ts'
+  'sonar.typescript.lcov.reportPaths'='coverage/frontend/lcov.info'
        },
 }, () => {});
